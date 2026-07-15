22fe2d0db1def70edfc772806aa9f33ad853da2e
Misuki743 <goole0goole@gmail.com>
2026-07-15 11:18:40 +0800

    a

--- combi/stirling_second_kind.cpp
+++ combi/stirling_second_kind.cpp
@@ -8,7 +8,7 @@ FPS<Mint> stirling_second_kind(int n) {
   FPS<Mint> f(n + 1), g(n + 1);
   for(int i = 0; i <= n; i++) {
     f[i] = factorial_inv<Mint>(i) * (i % 2 == 1 ? -1 : 1);
-    g[i] = factorial_inv<Mint>(i) * mint(i).pow(n);
+    g[i] = factorial_inv<Mint>(i) * Mint(i).pow(n);
   }
   auto h = f * g;
   h.resize(n + 1);
