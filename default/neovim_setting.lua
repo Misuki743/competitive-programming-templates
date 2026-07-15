f846c0277e12b7551adb8445e459d99cd625ed82
Misuki743 <goole0goole@gmail.com>
2026-07-15 11:29:31 +0800

    a

---
+++ default/neovim_setting.lua
@@ -0,0 +1,67 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.cmd('set autoindent')
+vim.cmd('set tabstop=2')
+vim.cmd('set shiftwidth=2')
+vim.cmd('set expandtab')
+vim.cmd('set relativenumber')
+vim.cmd('syntax on')
+vim.cmd('map<C-c> :!clang++ -DDEBUG -Wall -Wextra -Wshadow -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -O2 -fsanitize=address,undefined -g -o %:r -std=c++20 %<CR>')
+vim.cmd('map<C-v> :!g++-15 -O2 -o %:r -std=c++20 %<CR>')
+
+vim.cmd([[
+function SetDefaultContext()
+  :r ~/Documents/CP/template/default/tt.cpp
+  :1,1d
+  :call cursor(650, 2)
+  :normal zz
+endfunction]])
+vim.cmd('autocmd BufNewFile *.cpp :call SetDefaultContext()')
+
+vim.cmd('set termguicolors')
+vim.cmd('colorscheme nightfox')
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'nightfly',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'filetype'},
+    lualine_y = {'location'},
+    lualine_z = {}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
+local palette = require('nightfox.palette').load('nightfox')
+local Color = require("nightfox.lib.color")
+
+local bg = Color.from_hex(palette.bg1)
+print(bg:to_css())
--- default/tt.cpp
+++ default/tt.cpp
@@ -270,7 +270,69 @@ T ceilDiv(T a, T b) {
 template<class T> bool chmin(T &a, T b) { return a > b ? a = b, 1 : 0; }
 template<class T> bool chmax(T &a, T b) { return a < b ? a = b, 1 : 0; }
 
-//constantly used templates
+template<class Mint>
+Mint factorial(int n) {
+  static vc<Mint> dat;
+  if (n >= ssize(dat)) {
+    if (dat.empty()) dat.eb(1);
+    int size0 = ssize(dat);
+    dat.resize(min(Mint::get_mod(), bit_ceil((uint32_t)(n + 1))));
+    for(int i = size0; i < ssize(dat); i++)
+      dat[i] = dat[i - 1] * i;
+  }
+  return dat[n];
+}
+
+template<class Mint>
+Mint factorial_inv(int n) {
+  static vc<Mint> dat;
+  if (n >= ssize(dat)) {
+    int size0 = ssize(dat);
+    dat.resize(min(Mint::get_mod(), bit_ceil((uint32_t)(n + 1))));
+    dat.back() = factorial<Mint>(ssize(dat) - 1).inverse();
+    for(int i = ssize(dat) - 2; i >= size0; i--)
+      dat[i] = dat[i + 1] * (i + 1);
+  }
+  return dat[n];
+}
+
+template<class Mint>
+Mint inverse(int n) {
+  return factorial_inv<Mint>(n) * factorial<Mint>(n - 1);
+}
+
+template<class Mint>
+Mint binomial(int n, int k) {
+  if (0 <= k and k <= n)
+    return factorial<Mint>(n) * factorial_inv<Mint>(k) * factorial_inv<Mint>(n - k);
+  else
+    return Mint(0);
+}
+
+template<class Mint>
+Mint catalan(int n) {
+  return binomial<Mint>(2 * n, n) - binomial<Mint>(2 * n, n + 1);
+}
+
+//number of up-down path with n (+1), m (-1) and never touch y = -k
+template<class Mint>
+Mint excatalan(int n, int m, int k) {
+  if (k > m) return binomial<Mint>(n + m, m);
+  else if (k > m - n) return binomial<Mint>(n + m, m) - binomial<Mint>(n + m, m - k);
+  else return Mint(0);
+}
+
+template<class Mint>
+auto binomial_functions() {
+  return tuple(
+    &factorial<Mint>,
+    &factorial_inv<Mint>,
+    &inverse<Mint>,
+    &binomial<Mint>,
+    &catalan<Mint>,
+    &excatalan<Mint>
+  );
+}
 
 struct HLD {
   int n, root;
@@ -580,7 +642,7 @@ struct Montgomery_modint {
 
 //using mint = Montgomery_modint<1'000'000'007>;
 using mint = Montgomery_modint<998'244'353>;
-
+auto [fac, faci, inv, binom, cat, excat] = binomial_functions<mint>();
 
 signed main() {
   ios::sync_with_stdio(false), cin.tie(NULL);
