61edb3f55fc30750b74edc5b4949826716364ba1
Misuki743 <goole0goole@gmail.com>
2026-07-26 22:18:47 +0800

    a

---
+++ numtheory/Lucy_DP.cpp
@@ -0,0 +1,62 @@
+//Given a multiplicative function f s.t.
+//- F(n), f(p^e) can be calculated quickly
+//- f(p) = f'(p) for all p
+//
+//Return F'_p(n) for all n in Q_N
+template<class T, int32_t SQRT_N, typename F1, typename F2>
+requires R_invocable<T, F1, ll> && R_invocable<T, F2, ll, ll, ll>
+vc<T> Lucy_DP(ll N, F1 F, F2 f) {
+  static prime_table<SQRT_N + 1> pt;
+
+  ll x = sqrtl(N);
+  while(x * (x + 1) <= N) x++;
+  ll m = N / x;
+
+  auto id = [&](ll n) { return n <= m ? n - 1 : m + x - 1 - N / n; };
+  vc<T> dp;
+  vll qs;
+  dp.reserve(m + x - 1), qs.reserve(m + x - 1);
+  for(ll i = 1; i < m; i++) dp.eb(F(i)), qs.eb(i);
+  for(ll i = x; i >= 1; i--) dp.eb(F(N / i)), qs.eb(N / i);
+
+  for(ll p = 2; p * p <= N; p++) if (pt.is_prime(p)) {
+    for(ll i = m + x - 2; i >= 0 and qs[i] >= p * p; i--)
+      dp[i] += f(p, 1, p) * (dp[p - 2] - dp[id(qs[i] / p)]);
+    /* for the case where f is not completely multiplicative (when would this be useful?)
+    ll i = m + x - 2;
+    while(i > 0 and qs[i - 1] >= p * p) i--;
+    for(; i < m + x - 1; i++) {
+      for(ll q = p, e = 1; q * (ull)p <= qs[i]; q *= p, e++) {
+        dp[i] -= f(p, e + 1, q * p);
+        dp[i] += f(p, e, q) * (dp[p - 1] - dp[id(qs[i] / q)]);
+      }
+    }
+    */
+  }
+
+  return dp;
+}
+
+template<class T, int32_t SQRT_N, typename F1>
+requires R_invocable<T, F1, ll, ll, ll> 
+vc<T> inverse_Lucy_DP(ll N, vc<T> Fp, F1 f) {
+  static prime_table<SQRT_N + 1> pt;
+
+  ll x = sqrtl(N);
+  while(x * (x + 1) <= N) x++;
+  ll m = N / x;
+
+  auto id = [&](ll n) { return n <= m ? n - 1 : m + x - 1 - N / n; };
+  vll qs;
+  qs.reserve(m + x - 1);
+  for(ll i = 1; i < m; i++) qs.eb(i);
+  for(ll i = x; i >= 1; i--) qs.eb(N / i);
+
+  for(ll p = m; p > 1; p--) if (pt.is_prime(p)) {
+    for(ll i = m + x - 2; i >= 0 and qs[i] >= p * p; i--)
+      for(ll e = 1, q = p; q * (ull)p <= qs[i]; q *= p, e++)
+        Fp[i] += f(p, e, q) * (Fp[id(qs[i] / q)] - Fp[p - 1]) + f(p, e + 1, q * p);
+  }
+
+  return Fp;
+}
---
+++ test/sum_of_multiplicative_function.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/sum_of_multiplicative_function"
+
+#include "../default/t.cpp"
+#include "../modint/Montgomery_modint.cpp"
+#include "../numtheory/prime_table.cpp"
+#include "../numtheory/Lucy_DP.cpp"
+
+using Mint = Montgomery_modint<469'762'049>;
+
+int main() {
+  ios::sync_with_stdio(false), cin.tie(NULL);
+
+  int t; cin >> t;
+  while(t--) {
+    ll N, a, b; cin >> N >> a >> b;
+    auto Fp = Lucy_DP<Mint, (1 << 20)>(N, [](ll x) { return Mint(x) * (x + 1) * ((Mint::get_mod() + 1) / 2) - 1; }, [](ll, ll, ll q) { return Mint(q); });
+    auto Fp2 = Lucy_DP<Mint, (1 << 20)>(N, [](ll x) { return Mint(x) - 1; }, [](ll, ll, ll) { return Mint(1); });
+    for(int i = 0; i < ssize(Fp); i++)
+      Fp[i] = a * Fp2[i] + b * Fp[i];
+
+    cout << 1 + inverse_Lucy_DP<Mint, (1 << 20)>(N, Fp, [&](ll p, ll e, ll) -> Mint { return Mint(a) * e + Mint(b) * p; }).back() << '\n';
+  }
+
+  return 0;
+}
