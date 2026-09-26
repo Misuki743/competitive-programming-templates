b8a1dbd8aea85f50ec6c72539d30dddff0e70244
Misuki743 <goole0goole@gmail.com>
2026-09-26 15:33:30 +0800

    a

--- ds/binary_trie.cpp
+++ ds/binary_trie.cpp
@@ -1,59 +1,68 @@
-template<int mxBit, bool duplicate = false>
+template<int W, bool duplicate = false>
 struct binary_trie {
-  vector<array<int, 2>> nxt;
-  vector<int> cnt;
+  vc<array<int32_t, 2>> nxt;
+  vi cnt;
 
-  binary_trie(int size = 0) : nxt(1, {-1, -1}), cnt(1) {
-    nxt.reserve(size);
-    cnt.reserve(size);
+  binary_trie(int size = 1) : nxt(1, {-1, -1}), cnt(1) {
+    nxt.reserve(size), cnt.reserve(size);
   }
 
   int count(ull x) {
     int v = 0;
-    for(int bit = mxBit; bit >= 0; bit--) {
-      ull to = x >> bit & 1;
-      if (nxt[v][to] == -1) return 0;
-      v = nxt[v][to];
+    for(int i = W - 1; i >= 0; i--) {
+      v = nxt[v][x >> i & 1];
+      if (v == -1) return 0;
     }
     return cnt[v];
   }
 
   void insert(ull x) {
-    if constexpr (!duplicate) {
-      if (count(x)) return;
-    }
+    if constexpr (!duplicate)
+      if (count(x))
+        return;
     int v = 0;
-    cnt[0] += 1;
-    for(int bit = mxBit; bit >= 0; bit--) {
-      ull to = x >> bit & 1;
-      if (nxt[v][to] == -1) {
-        nxt[v][to] = ssize(nxt);
-        nxt.push_back({-1, -1});
-        cnt.emplace_back();
+    cnt[0]++;
+    for(int i = W - 1; i >= 0; i--) {
+      if (nxt[v][x >> i & 1] == -1) {
+        nxt[v][x >> i & 1] = ssize(nxt);
+        nxt.pb({-1, -1}), cnt.eb();
       }
-      v = nxt[v][to], cnt[v] += 1;
+      v = nxt[v][x >> i & 1], cnt[v]++;
     }
   }
 
   void erase(ull x) {
     if (!count(x)) return;
     int v = 0;
-    cnt[0] -= 1;
-    for(int bit = mxBit; bit >= 0; bit--)
-      v = nxt[v][x >> bit & 1], cnt[v] -= 1;
+    cnt[0]--;
+    for(int i = W - 1; i >= 0; i--)
+      v = nxt[v][x >> i & 1], cnt[v]--;
   }
 
-  ull query_min(ull Xor = 0LL) {
+  ull query_min(ull XOR = 0ull) {
     assert(cnt[0] > 0);
-    ull res = 0;
-    int v = 0;
-    for(int bit = mxBit; bit >= 0; bit--) {
-      ull to = Xor >> bit & 1;
-      if (nxt[v][to] != -1 and cnt[nxt[v][to]] >= 1)
+    ull ans = 0;
+    for(int i = W - 1, v = 0; i >= 0; i--) {
+      ull to = XOR >> i & 1;
+      if (nxt[v][to] != -1 and cnt[nxt[v][to]] > 0)
         v = nxt[v][to];
       else
-        res |= 1LL << bit, v = nxt[v][to ^ 1];
+        ans |= 1ull << i, v = nxt[v][to ^ 1];
     }
-    return res;
+    return ans;
+  }
+
+  ull kth(int k, ull XOR = 0ull) {
+    assert(cnt[0] > k);
+    ull ans = 0;
+    for(int i = W - 1, v = 0; i >= 0; i--) {
+      ull to = XOR >> i & 1;
+      if (nxt[v][to] != -1 and cnt[nxt[v][to]] <= k)
+         k -= cnt[nxt[v][to]], to ^= 1;
+      else if (nxt[v][to] == -1)
+        to ^= 1;
+      v = nxt[v][to], ans |= to << i;
+    }
+    return ans ^ XOR;
   }
 };
--- test/set_xor_min.test.cpp
+++ test/set_xor_min.test.cpp
@@ -6,7 +6,7 @@
 int main() {
   ios::sync_with_stdio(false), cin.tie(NULL);
 
-  binary_trie<29> tr(500'000 * 30);
+  binary_trie<30> tr(500'000 * 30 + 1);
 
   int q; cin >> q;
   while(q--) {
